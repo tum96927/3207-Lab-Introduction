a8d8f36f4231f57b2c18adba37484bd7bf3b5d37
Brandon Zheng <tum96927@temple.edu>
2023-01-18 15:32:22 -0500

    Added random.c

--- random.c
+++ random.c
@@ -1,7 +1,10 @@
+#include <stdlib.h>
+#include "random.h"
 //Fuction Prototype
 int randchar();
 
 int randchar(){
     //ASCII A -> Z : 65 - 90
-
+    int random = 65 + (rand() % 26);
+    return random;
 }
