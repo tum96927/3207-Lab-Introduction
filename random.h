28b73349c1bae19efd63b2a7071a9071f10168b0
Brandon Zheng <tum96927@temple.edu>
2023-01-18 15:34:38 -0500

    Added random.h

---
+++ random.h
@@ -0,0 +1,8 @@
+//
+// Created by Brandon Zheng on 1/18/23.
+//
+
+#ifndef INC_3207_LAB_INTRODUCTION_RANDOM_H
+#define INC_3207_LAB_INTRODUCTION_RANDOM_H
+int randchar();
+#endif //INC_3207_LAB_INTRODUCTION_RANDOM_H
